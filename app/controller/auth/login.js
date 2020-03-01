78cf4b5df7cd370034155c43531a1b421442a274
Leonardo Landin <leolandin123@gmail.com>
2020-03-01 16:19:42 -0300

    search user in mongodb

--- app/controller/auth/login.js
+++ app/controller/auth/login.js
@@ -1,10 +1,40 @@
-const LoginAPI = require('../../dao/LoginDAO')
+const LoginDAO = require('../../dao/LoginDAO')
 
 module.exports = (req, res) => {
-    res.send('gorila')
-    console.log(req.params.dataUser)
-    LoginAPI.getUserByEmail({teste: 'name'}).then((data) => {
-        console.log(data)
-    })
+    res.header("Access-Control-Allow-Origin", "*");
+    const ValidationException = (message, response) => {
+        let sendObject = {
+            statusCode: 401,
+            message: message
+        }
+        response.send(sendObject)
+    }
+    
+    let dataUser = JSON.parse(req.params.dataUser);
 
+    if(dataUser.user && dataUser.pass) {
+        if(dataUser.user.length > 254) {
+            return ValidationException('O e-mail não pode conter mais de 254 caracteres', res)
+        }
+        if(dataUser.pass.length > 128) {
+            return ValidationException('A senha não pode conter mais de 128 caracteres', res)
+        }
+        if(!dataUser.user.includes('@')) {
+            return ValidationException('O e-mail inserido não contém um formato de e-mail válido (@)', res)
+        } 
+        
+        LoginDAO.getUserByEmail(dataUser.user).then((data) => {
+            let dataDAO = data
+            if(dataDAO === null) {
+               return ValidationException('Não existe um usuário com esse e-mail', res) 
+            }
+            console.log(dataDAO)
+            console.log(dataDAO.email)
+            console.log()
+            if(dataUser.user == dataDAO.email && dataUser.pass == dataDAO.password) {
+                res.send('perdemos')
+            }
+        })
+
+    }
 }
--- app/dao/LoginDAO.js
+++ app/dao/LoginDAO.js
@@ -3,7 +3,7 @@ const User = require('../model/UserModel')
 const LoginAPI = {}
 
 LoginAPI.getUserByEmail = async function(objCredentials) {
-    return User.findOne({}, (err, result) => {
+    return User.findOne({'email': objCredentials}, (err, result) => {
         return new Promise((resolve, reject) => {
             resolve(result)
         })
