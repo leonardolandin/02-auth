b8e7b2cc0ce8167539efd63de448b0f17fd9f589
leonardolandin <leonardo.simoes@reclameaqui.com.br>
2020-03-29 17:50:40 -0300

    escape

--- app/controller/auth/register.js
+++ app/controller/auth/register.js
@@ -1,10 +1,35 @@
 const AuthDAO = require('../../dao/AuthDAO');
+require('dotenv/config');
 
 
 module.exports = (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
+
+    const ValidateCredentials = (objUser) => {
+        if(objUser && objUser.email && objUser.email.length > 254) {
+            return ValidationException('O e-mail não pode conter mais de 254 caracteres', res)
+        }
+        if(objUser && objUser.email && !objUser.email.includes('@')) {
+            return ValidationException('O e-mail inserido não contém um formato de e-mail válido (@)', res)
+        }
+        if(objUser && objUser.name && objUser.name.length > 120) {
+            return ValidationException('O nome não pode conter mais de 120 caracteres', res)
+        }
+
+        return true;
+    }
+
+    const ValidationException = (message, response) => {
+        let sendObject = {
+            statusCode: 401,
+            message: message
+        }
+        response.send(sendObject)
+    }
+
     let dataUser = JSON.parse(req.params.dataNewUser);
-    
-    console.log(dataUser.name);
-    res.send('pinto')
+
+    if(dataUser && ValidateCredentials(dataUser)) {
+        
+    }
 }
